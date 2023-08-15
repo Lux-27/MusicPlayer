4aedb34723bffa454b09e8fcaa0400bcb02c3e72
unknown <raj170303@gmail.com>
2023-08-15 19:02:41 +0530

    added profile picture

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./sidebar.css";
 import SidebarButton from "./sidebarButton";
 
@@ -8,14 +8,23 @@ import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
 
+import apiClient from "../../spotify";
+
 export default function Sidebar() {
+  const [image, setImage] = useState(
+    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+  );
+
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      // console.log(response.data.images[0].url);
+      setImage(response.data.images[0].url);
+    });
+  }, []);
+
   return (
     <div className="sidebar-container">
-      <img
-        src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-        className="profile-image"
-        alt="profile"
-      ></img>
+      <img src={image} className="profile-image" alt="profile"></img>
       <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Library from "../library/library";
 import Feed from "../feed/feed";
@@ -7,9 +7,35 @@ import Player from "../player/player";
 import Favourites from "../favourites/favourites";
 import "./home.css";
 import Sidebar from "../../components/sidebar/sidebar";
+import Login from "../auth/login";
+import { setClientToken } from "../../spotify";
 
 export default function Home() {
-  return (
+  const [token, setToken] = useState("");
+
+  //window.location.hash gives the access token from the browser url
+  //split it based on &, take first element from array to get accessToken
+  //accessToken looks like accessToken = sadfada
+  //split based on '=' and take first index to get access token
+  //store in local storage with key of token
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const accessToken = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", accessToken);
+      setToken(accessToken);
+      setClientToken(accessToken);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+
+  return !token ? (
+    <Login />
+  ) : (
     <Router>
       <div className="main-body">
         <Sidebar />
