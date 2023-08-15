84b9c598463881fb5177d11a34c4e369e3223fa6
unknown <raj170303@gmail.com>
2023-08-15 20:54:25 +0530

    playlist added

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import apiClient from "../../spotify";
+import "./library.css";
+import { IconContext } from "react-icons";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 export default function Library() {
-  return <div className="screen-container">Library</div>;
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    apiClient.get("me/playlists").then(function (response) {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
+    });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const playPlaylist = (id) => {
+    navigate("/player", { state: { id: id } });
+  };
+
+  return (
+    <div className="screen-container">
+      <div className="library-body">
+        {playlists?.map((playlist) => (
+          <div
+            className="playlist-card"
+            key={playlist.id}
+            onClick={() => playPlaylist(playlist.id)}
+          >
+            <img
+              src={playlist.images[0].url}
+              className="playlist-image"
+              alt="playlist-art"
+            />
+            <p className="playlist-title">{playlist.name}</p>
+            <p className="playlist-subtitle">{playlist.tracks.total} songs</p>
+            <div className="playlist-fade">
+              <IconContext.Provider value={{ size: "50px", color: "#e99d72" }}>
+                <AiFillPlayCircle />
+              </IconContext.Provider>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
